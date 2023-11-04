88aaed0890debba138e33f4767753a6a92cf70f0
omarbenjdidia <92368449+omarbenjdidia@users.noreply.github.com>
2023-11-04 17:34:39 +0100

    Update and rename UserServiceImplTest.java to EtudiantServiceImplTest.java

---
+++ src/test/java/tn/esprit/spring/services/EtudiantServiceImplTest.java
@@ -0,0 +1,27 @@
+package tn.esprit.spring.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import tn.esprit.spring.entities.User;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+class UserServiceImplTest {
+
+    @Autowired
+    IUserService us;
+
+    @Test
+    @Order(1)
+    public void testRetrieveAllUsers() {
+        List<User> listUsers = us.retrieveAllUsers();
+        Assertions.assertEquals(0, listUsers.size());
+    }
+}
